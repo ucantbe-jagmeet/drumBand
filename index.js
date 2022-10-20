7d4ffee979e7980af1cae38b73953c510565e460
Jagmeet Singh <ucantbe-jagmeet>
2022-10-20 17:28:09 +0530

    Drum Band Project

---
+++ index.js
@@ -0,0 +1,82 @@
+
+
+var j=document.querySelectorAll(".drum").length;
+for(var i=0;i<j;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        var buttonInnerHtml=this.innerHTML;
+       makeSound(buttonInnerHtml);
+       buttonAnimation(buttonInnerHtml);
+    });
+}
+
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var tom1 = new Audio('sounds\\tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio('sounds\\tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio('sounds\\tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio('sounds\\tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio('sounds\\snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio('sounds\\crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio('sounds\\kick-bass.mp3');
+            kick.play();
+            break;
+
+        default: console.log(this.innerHTML);
+    }   
+
+}
+
+function buttonAnimation(currentKey){
+        var activeButton=document.querySelector("."+currentKey);
+        activeButton.classList.add("pressed");
+
+        setTimeout(function(){
+            activeButton.classList.remove("pressed");
+        }, 100);
+}
+
+/*for(i = 0; i < document.querySelectorAll('.drum').length;i++){    
+
+    document.querySelectorAll('.drum')[i].addEventListener('click', function(){
+
+    this.style.color = "white";    
+
+     });
+
+}/*
+for(var i=0;i<j;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+       this.innerHtml.style.color="white";
+    });
+}
+
+/*var audio = new Audio('sounds\\tom-1.mp3');
+        audio.play();*/
